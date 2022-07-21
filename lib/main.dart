3ceb8af656f8efa71f8ddd8786332c90c80c6801
zzunho9 <jamlove4763@naver.com>
2022-07-21 15:21:04 +0900

    design: add birthday friends rows

--- lib/main.dart
+++ lib/main.dart
@@ -41,54 +41,79 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.white,
           elevation: 0,
         ),
-        body: Column(
-          children: [
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
-                child: Column(
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Container(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(25.0),
-                            child: Image.asset('assets/empty_profile.jpg', width: 60, height: 60, ),
-                          ),
-                        ),
-                        SizedBox(width: 15,),
-                        Text("준호", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),),
-                      ],
+                    Container(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(25.0),
+                        child: Image.asset('assets/empty_profile.jpg', width: 60, height: 60, ),
+                      ),
                     ),
-                    SizedBox(height: 10,),
-                    Divider(thickness: 2, color: Color(0xfffafafa),),
+                    SizedBox(width: 15,),
+                    Text("준호", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),),
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Divider(thickness: 2, color: Color(0xfffafafa),),
+                SizedBox(height: 5,),
+                Row(
+                  children: const [
+                    Text("내 멀티프로필", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  children: [
                     SizedBox(height: 5,),
-                    Row(
-                      children: [
-                        Text("내 멀티프로필", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w200, fontSize: 12),),
-                      ],
+                    Container(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(18.0),
+                        child: Image.asset('assets/plus_icon.jpg', width: 45, height: 45, ),
+                      ),
                     ),
-                    SizedBox(height: 5,),
-                    Row(
-                      children: [
-                        SizedBox(height: 5,),
-                        Container(
-                          child: ClipRRect(
+                    SizedBox(width: 10,),
+                    Text("친구별로 다른 프로필을 설정해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Divider(thickness: 2, color: Color(0xfffafafa),),
+                SizedBox(height: 5,),
+                Row(
+                  children: const [
+                    Text("생일인 친구", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          ClipRRect(
                             borderRadius: BorderRadius.circular(18.0),
-                            child: Image.asset('assets/plus_icon.jpg', width: 45, height: 45, ),
+                            child: Image.asset('assets/birthday_cake.jpg', width: 45, height: 45, ),
                           ),
-                        ),
-                        SizedBox(width: 10,),
-                        Text("친구별로 다른 프로필을 설정해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
-                      ],
+                          SizedBox(width: 10,),
+                          Text("친구의 생일을 확인해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Text("5 ⟩", style: TextStyle(color: Colors.grey),)
                     )
                   ],
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
-          bottomNavigationBar: BottomNavigationBar(
+        bottomNavigationBar: BottomNavigationBar(
               type: BottomNavigationBarType.fixed,
               backgroundColor: Color(0xfff9f9f9),
               onTap: (index) => {},
