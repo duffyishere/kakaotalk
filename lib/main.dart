424f58854d6fb365dbaf94dd87e617c180e770c8
zzunho9 <jamlove4763@naver.com>
2022-07-21 16:26:33 +0900

    design: add friends list

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          resizeToAvoidBottomInset : false,
+          appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -41,138 +42,204 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.white,
           elevation: 0,
         ),
-        body: Container(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(25.0),
-                        child: Image.asset('assets/empty_profile.jpg', width: 60, height: 60, ),
-                      ),
-                    ),
-                    SizedBox(width: 15,),
-                    Text("준호", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),),
-                  ],
-                ),
-                Column(
-                  children: const [
-                    SizedBox(height: 10,),
-                    Divider(thickness: 2, color: Color(0xfffafafa),),
-                    SizedBox(height: 5,),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Text("내 멀티프로필", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
-                  ],
-                ),
-                SizedBox(height: 5,),
-                Row(
-                  children: [
-                    SizedBox(height: 5,),
-                    Container(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(18.0),
-                        child: Image.asset('assets/plus_icon.jpg', width: 45, height: 45, ),
-                      ),
-                    ),
-                    SizedBox(width: 10,),
-                    Text("친구별로 다른 프로필을 설정해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
-                  ],
-                ),
-                Column(
-                  children: const [
-                    SizedBox(height: 10,),
-                    Divider(thickness: 2, color: Color(0xfffafafa),),
-                    SizedBox(height: 5,),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Text("생일인 친구", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
-                  ],
-                ),
-                SizedBox(height: 5,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(18.0),
-                            child: Image.asset('assets/birthday_cake.jpg', width: 45, height: 45, ),
-                          ),
-                          SizedBox(width: 10,),
-                          Text("친구의 생일을 확인해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                      child: Text("5 ⟩", style: TextStyle(color: Colors.grey),)
-                    )
-                  ],
-                ),
-                Column(
-                  children: const [
-                    SizedBox(height: 5,),
-                    Divider(thickness: 2, color: Color(0xfffafafa),),
-                    SizedBox(height: 10,),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Text("업데이트한 친구 10", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
-                  ],
-                ),
-                SizedBox(height: 10,),
-                Container(
-                  child: Row(
+        body: SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+              child: Column(
+                children: [
+                  // 내 프로팔
+                  Row(
                     children: [
-                      Expanded(flex: 100, child: Image.asset('assets/new_profiles.jpg',),)
+                      Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Image.asset('assets/empty_profile.jpg', width: 60, height: 60, ),
+                        ),
+                      ),
+                      SizedBox(width: 15,),
+                      Text("준호", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),),
                     ],
                   ),
-                ),
-                Column(
-                  children: const [
-                    SizedBox(height: 5,),
-                    Divider(thickness: 2, color: Color(0xfffafafa),),
-                    SizedBox(height: 10,),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Text("추천친구", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
-                  ],
-                ),
-                SizedBox(height: 5,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Row(
+                  Column(
+                    children: const [
+                      SizedBox(height: 10,),
+                      Divider(thickness: 2, color: Color(0xfffafafa),),
+                      SizedBox(height: 5,),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text("내 멀티프로필", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                    ],
+                  ),
+                  // 내 멀티프로핑
+                  SizedBox(height: 5,),
+                  Row(
+                    children: [
+                      SizedBox(height: 5,),
+                      Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(18.0),
+                          child: Image.asset('assets/plus_icon.jpg', width: 45, height: 45, ),
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Text("친구별로 다른 프로필을 설정해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      SizedBox(height: 10,),
+                      Divider(thickness: 2, color: Color(0xfffafafa),),
+                      SizedBox(height: 5,),
+                    ],
+                  ),
+                  // 생일인 친구
+                  Row(
+                    children: const [
+                      Text("생일인 친구", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(18.0),
+                              child: Image.asset('assets/birthday_cake.jpg', width: 45, height: 45, ),
+                            ),
+                            SizedBox(width: 10,),
+                            Text("친구의 생일을 확인해보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                          ],
+                        ),
+                      ),
+                      Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                          child: Text("5 ⟩", style: TextStyle(color: Colors.grey),)
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      SizedBox(height: 5,),
+                      Divider(thickness: 2, color: Color(0xfffafafa),),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                  // 업데이트한 친구
+                  Row(
+                    children: const [
+                      Text("업데이트한 친구 10", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    child: Row(
+                      children: [
+                        Expanded(flex: 100, child: Image.asset('assets/new_profiles.jpg',),)
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: const [
+                      SizedBox(height: 5,),
+                      Divider(thickness: 2, color: Color(0xfffafafa),),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                  // 추천친구
+                  Row(
+                    children: const [
+                      Text("추천친구", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(18.0),
+                              child: Image.asset('assets/new_friends.jpg', width: 45, height: 45, ),
+                            ),
+                            SizedBox(width: 10,),
+                            Text("새로운 친구를 만나보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                          ],
+                        ),
+                      ),
+                      Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                          child: Text("100 ⟩", style: TextStyle(color: Colors.grey),)
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      SizedBox(height: 5,),
+                      Divider(thickness: 2, color: Color(0xfffafafa),),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                  // 채널
+                  Row(
+                    children: const [
+                      Text("채널", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w100, fontSize: 12),),
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Column(
+                    children: const [
+                      SizedBox(height: 5,),
+                      Divider(thickness: 2, color: Color(0xfffafafa),),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                  //친구
+                  Row(
+                    children: const [
+                      Text("친구 121", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w200, fontSize: 12),),
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Column(
+                    children: [
+                      Row(
                         children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(18.0),
-                            child: Image.asset('assets/new_friends.jpg', width: 45, height: 45, ),
+                          SizedBox(height: 5,),
+                          Container(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(18.0),
+                              child: Image.asset('assets/profile1.jpg', width: 45, height: 45, ),
+                            ),
                           ),
-                          SizedBox(width: 10,),
-                          Text("새로운 친구를 만나보세요!", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                          SizedBox(width: 15,),
+                          Text("강예준", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
                         ],
                       ),
-                    ),
-                    Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                        child: Text("100 ⟩", style: TextStyle(color: Colors.grey),)
-                    )
-                  ],
-                ),
-              ],
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          SizedBox(height: 5,),
+                          Container(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(18.0),
+                              child: Image.asset('assets/empty_profile_small.jpg', width: 45, height: 45, ),
+                            ),
+                          ),
+                          SizedBox(width: 15,),
+                          Text("강래연", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15.5),),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
